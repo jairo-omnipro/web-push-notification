d237f2863fe591dbe65be3d8d178a1e9e8a70c4b
jairo-omnipro <jairo.guevara@omni.pro>
2023-03-29 16:15:20 -0500

    p

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,16 @@
-importScripts('https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/9.17.2/firebase-messaging.js')
+importScripts("https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
-firebase.initializeApp({
-    messagingSenderId: "1055055539691"
-})
+const firebaseConfig = {
+    apiKey: "AIzaSyAAETyJlBnNRkm4rPJn4Jiebn5xmbvzfQg",
+    authDomain: "marketo-webhooks.firebaseapp.com",
+    projectId: "marketo-webhooks",
+    storageBucket: "marketo-webhooks.appspot.com",
+    messagingSenderId: "1055055539691",
+    appId: "1:1055055539691:web:0f4c70586ab562cefbbdae"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging()
 
